1faeb87abc2f3d8efd6a13a3189d6c133e96d5f4
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-08-09 16:23:47 +0100

    Start a script

---
+++ markdown2html.py
@@ -0,0 +1,24 @@
+import sys
+import markdown
+
+def convert_markdown_to_html(input_file, output_file):
+    try:
+        with open(input_file, 'r') as md_file:
+            markdown_content = md_file.read()
+            html_content = markdown.markdown(markdown_content)
+            with open(output_file, 'w') as html_file:
+                html_file.write(html_content)
+    except FileNotFoundError:
+        sys.stderr.write(f"Missing {input_file}\n")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+    
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    convert_markdown_to_html(input_file, output_file)
+    sys.exit(0)
