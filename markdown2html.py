cc3079986c2a453c91c515f25e88a62ba1ce62c7
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-08-10 13:18:35 +0100

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-'''
-    This script convert markdown file into html file
-    This script convert markdown file into html file
-    This script convert markdown file into html file
-    This script convert markdown file into html file
-    This script convert markdown file into html file
-    This script convert markdown file into html file
-    This script convert markdown file into html file
-    This script convert markdown file into html file
-    This script convert markdown file into html file
+''' This script convert markdown file into html file
 '''
 
 import sys
