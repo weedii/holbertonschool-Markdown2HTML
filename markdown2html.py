6056918a52440e991a0fbd5d9076a61590d9cbea
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-08-11 23:17:08 +0100

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,33 +1,46 @@
 #!/usr/bin/python3
-
-"""Write a script markdown2html.py that takes an argument 2 strings:
-
-First argument is the name of the Markdown file
-Second argument is the output file name"""
-
+"""Write a script markdown2html.py that takes an argument 2 strings:"""
 import sys
-import markdown
+
+def convert_heading(line):
+    if line.startswith("#"):
+        heading_level = min(line.count("#"), 6)
+        heading_text = line.strip("# ").strip()
+        html_heading = f"<h{heading_level}>{heading_text}</h{heading_level}>"
+        return html_heading
+    else:
+        return line
+def convert_unordered_list(line):
+    if line.startswith("- ") and not line.startswith("# "):
+        list_item = line.strip("- ").strip()
+        html_list_item = f"<li>{list_item}</li>"
+        return html_list_item
+    else:
+        return line
 
 
-def convert_markdown_to_html(input_file, output_file):
+def markdown_file(name, output):
     try:
-        with open(input_file, 'r') as md_file:
-            markdown_content = md_file.read()
-            html_content = markdown.markdown(markdown_content)
-            with open(output_file, 'w') as html_file:
-                html_file.write(html_content)
+        with open(name, 'r') as file:
+            markdown_lines = file.readlines()
+
+        converted_lines = []
+        for line in markdown_lines:
+            converted_line = convert_heading(line)
+            converted_line = convert_unordered_list(converted_line)
+            converted_lines.append(converted_line)
+
+        with open(output, 'w') as file:
+            for line in converted_lines:
+                file.write(line)
+
     except FileNotFoundError:
-        sys.stderr.write(f"Missing {input_file}\n")
+        sys.stderr.write(f"Missing {name}\n")
         sys.exit(1)
 
-
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    convert_markdown_to_html(input_file, output_file)
-    sys.exit(0)
+    markdown_file(sys.argv[1], sys.argv[2])
