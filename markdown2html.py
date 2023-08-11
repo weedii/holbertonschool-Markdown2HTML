5336fff49e8a008463ebaa8b873b4ec795451ee9
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-08-11 23:24:57 +0100

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""This script converts a markdown file into an html file."""
+"""This script convert a markdown file into html file"""
 
 import sys
 import markdown
 
-def convert_markdown_to_html(input_file, output_file):
-    """
-    Converts a markdown file to an HTML file.
 
-    Args:
-        input_file (str): Path to the input Markdown file.
-        output_file (str): Path to the output HTML file.
-    """
+def convert_markdown_to_html(input_file, output_file):
     try:
         with open(input_file, 'r') as md_file:
             markdown_content = md_file.read()
@@ -21,6 +15,7 @@ def convert_markdown_to_html(input_file, output_file):
     except FileNotFoundError:
         sys.stderr.write(f"Missing {input_file}\n")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
